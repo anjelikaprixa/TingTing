080967c7203d50dcb66458242ffdc74111ecb51e
Adwait Upadhyaya <64580218+adwaitupadhyaya@users.noreply.github.com>
2023-05-18 16:56:03 +0545

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,6 +29,6 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
-
+html_title = 'TingTing'
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
